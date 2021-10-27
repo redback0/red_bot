82e054d0ea4e59f85c6ec7c3e98953eff635f176
Nate Jackson <nate@thejackos.net>
2021-10-27 23:42:13 +1030

    Changed deposit.py: Fixed an issue that allowed you to overload the bank

--- commands/eco_commands/deposit.py
+++ commands/eco_commands/deposit.py
@@ -14,11 +14,20 @@ description = 'Put points into your bank. You can only use this once per day'
 servers = []
 
 
-bankMaxPer = 5
-walletMinPer = 0.1
+bankMaxMul = 5
+walletMinMul = 0.1
 
 # give a user 500 points, usable once per day
 async def _execute(bot, msg, path):
+
+	# check if there's a argument
+	if not ' ' in msg.content[msg.content.find(' ')+1:]:	
+		
+		log.info('User did not give an amount')
+		await msg.channel.send(
+			'How much would you like to deposit? (`all` for maximum amount)')
+
+
 	reload(eco_common)
 
 	userdata = eco_common.readFile(path, str(msg.author.id))
@@ -28,6 +37,12 @@ async def _execute(bot, msg, path):
 
 
 	today = datetime.date.today()
+	maxBank = userdata.get('walletMax') * bankMaxMul
+	minWallet = userdata.get('walletMax') * walletMinMul
+
+	if minWallet < 2000:
+		minWallet = 2000
+
 
 	# check if the user has already done this today
 	if not datetime.date.fromisoformat(
@@ -39,37 +54,33 @@ async def _execute(bot, msg, path):
 		return
 
 	# check if the user is able to store more in their bank
-	elif userdata.get('bank') >= userdata.get('walletMax') * bankMaxPer:
+	elif userdata.get('bank') >= maxBank:
 		log.info(
 			f'{user}\'s bank is already full: ' +
-			f'{userdata.get("bank")} >= {userdata.get("walletMax") * bankMaxPer}')
+			f'{userdata.get("bank")} >= {maxBank}')
 		await msg.channel.send('Your bank is already full')
 		return
 
 	# see if the user has enough in their wallet to deposit
-	elif (userdata.get('wallet') <= userdata.get('walletMax') * walletMinPer or 
+	elif (userdata.get('wallet') <= minWallet or 
 			userdata.get('wallet') <= 2000):
 		log.info(
 			f'{user}\'s wallet is too empty: {userdata.get("wallet")} ' + 
-			f'<= {userdata.get("walletMax") * walletMinPer}')
+			f'<= {minWallet}')
 		await msg.channel.send('Your wallet is too empty for this')
 		return
 
 
-	maxDep = int(userdata.get('wallet') - userdata.get('walletMax') * walletMinPer)
+	maxDep = int(userdata.get('wallet') - minWallet)
 
-	if userdata.get('wallet') - maxDep < 2000:
-		maxDep = userdata.get('wallet') - 2000
 
-	elif userdata.get('bank') + maxDep > userdata.get('walletMax') * bankMaxPer:
-		maxDep = userdata.get('walletMax') * bankMaxPer - userdata.get('bank')
+	if maxDep < userdata.get('wallet') - minWallet:
+		maxDep = userdata.get('wallet') - minWallet
 
-	# check if there's a argument
-	if not ' ' in msg.content[msg.content.find(' ')+1:]:	
-		
-		log.info('User did not give an amount')
-		await msg.channel.send(
-			'How much would you like to deposit? (`all` for maximum amount)')
+	if maxDep > maxBank - userdata.get('bank'):
+		maxDep = maxBank - userdata.get('bank')
+
+	
 
 
 	# set args list
