15d1254453e1e8dcae361e90db0c1616bfe6ab8b
Nathaniel JACKSON (001113651) <Nathaniel.JACKSON@student.tafesa.edu.au>
2021-11-24 15:53:03 +1030

    Added pickaxe.py

---
+++ commands/eco_commands/use_item/pickaxe.py
@@ -0,0 +1,17 @@
+import random
+
+async def use(qty):
+	total = 0
+	for i in range(qty):
+		total += _amount()
+
+
+
+
+def _amount():
+
+	# gets a random amount between 1 and 50
+	p1 = random.randrange(1, 22.4, 0.1)
+	p2 = random.randrange(1, 22.4, 0.1)
+
+	return (p1 * p2 / 100) # a number between 1 and 3.7^3
