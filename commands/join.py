706f8d2a568cb83a6ac5d64fb5b4a4dfa0b13b49
redback0 <nate@thejackos.net>
2023-12-27 14:50:46 +1030

    Made VC controls work with built in list of VoiceClients

--- commands/join.py
+++ commands/join.py
@@ -15,12 +15,8 @@ async def execute(bot, msg):
 	if not channel:
 		await msg.channel.send("Join a VC so I can join you!")
 	else:
-		if channel.permissions_for(msg.guild.me):
-			bot.voiceClients[msg.guild.id] = await channel.connect(self_deaf=True)
-			await msg.channel.send("Joined VC!")
-			log.info(f"Joined vc in {msg.guild.name}")
-		else:
-			await msg.channel.send("I don't have permission :cry:")
-			log.info(f"Unable to join VC: no permissoins")
+		await channel.connect(self_deaf=True)
+		await msg.channel.send("Joined VC!")
+		log.info(f"Joined vc in {msg.guild.name}")
 
 
