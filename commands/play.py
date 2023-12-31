8f54813cfa18d697fa7a2ef38359e30b61f46cdc
redback0 <nate@thejackos.net>
2023-12-31 18:46:48 +1030

    Made changes that probably work (can't test)

--- commands/play.py
+++ commands/play.py
@@ -1,6 +1,7 @@
 import logging
 import globs
 import discord
+import os
 from discord.utils import get
 
 logging.basicConfig()
@@ -14,7 +15,16 @@ permissions = "creator"
 async def execute(bot, msg):
 
 	voice_client = get(bot.voice_clients, guild=msg.guild)
+	log.debug(f"{msg.content.find(' ')} == {len(msg.content)}")
+	if msg.content.find(' ') < 0:
+		log.info("No audio source, setting default")
+		audio = "audio/default.mp3"
+		#SET DEFAULT AUDIO, OR ERROR?
+	else:
+		audio = f"audio/{msg.content[msg.content.find(' ') + 1:]}.mp3"
+	log.info(f"audio source: {audio}")
 
 	if voice_client and voice_client.is_connected():
-		source = await discord.FFmpegOpusAudio.from_probe("audio/test.mp3")
-		voice_client.play(source)
+		if (os.path.isfile(audio)):
+			source = await discord.FFmpegOpusAudio.from_probe(audio)
+			voice_client.play(source)
