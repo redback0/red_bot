44bc104fcbb913babd436ad086e5d233ca89233f
redback0 <nate@thejackos.net>
2024-01-11 16:38:35 +1030

    Removed file extension from mlist

--- commands/mlist.py
+++ commands/mlist.py
@@ -16,8 +16,7 @@ async def execute(bot, msg):
 	if os.path.isdir("audio"):
 		files = os.listdir("audio")
 		files.remove("default.mp3")
-		for file in files:
-			file = file[:-4]
+		files = [file[:-4] for file in files]
 		if len(files) > 0:
 			tracks = "```" + "\n".join(files) + "```"
 			await msg.channel.send(tracks)
